2d5fab1a0f565638855eccee3bd42f14ebf7be01
mri-nalini <39759399+mri-nalini@users.noreply.github.com>
2024-05-22 22:29:37 +0530

    Add files via upload

---
+++ Introduction_day.py
@@ -0,0 +1,52 @@
+# Hi Guys! 
+# Before we start with our python series,i Would like to tell you " Have a motto!"
+# Yes guys, starting things can be easy but to be motivated for same continously is a real task 
+
+# 3 things, i want you to do. 
+# > have a "AIM", learning python for what (data science or data analytics)
+# > Mention a time (under how many days,you would like to give yourself for completing it )
+# > Learn everyday (1 hour a day, you should gift yourself.)
+# > make a project 
+# With this let's start :
+
+## Basic arithmetic operations  * will take 2 Days
+# Control and contional structures
+# Looping- For, while loops 
+# User input 
+# strings
+# Integers
+# Floating Values 
+# Typecasting
+# This will take 2 Days 
+
+# Focus on in-buit function data structures * will take 3 Days
+#  > python strings and inbuit function
+#  > python list and boolean variables inbuit function
+#  > python sets- inbuilt functions
+#  > python Dictionaries-inbuit functions
+#  > python tuples- inbuit functions
+#  > function in python  (imp)
+#  > lambda Functions
+#  > iterators and Generators
+#  > Exception handling & imports Libraries
+
+# OOPS- All you need  * Will take 4 Days  2hr per day 
+# > classes
+# > object
+# > method
+# > inheritance
+# > polymorphic methods
+# > data Abstraction 
+# > Encapsulation 
+
+# Libraries with python  * Will take 2 Days
+# > numpy
+# > pandas
+# > matplotlib
+# > seaborn 
+# > Scipy 
+
+# path towards specialization (flask, django,dask,pytorch,keras)
+# > frameworks for web development (flask, django, web2py)
+# > fremeworks for building desktop application 
+# > further libraries for machine learning & deep learning 
